92db93bc2e7af4319f230e621a1e57231a6bc7db
Narlotl <78450405+Narlotl@users.noreply.github.com>
2021-10-04 13:11:57 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.querySelector('button#go').onclick = function () {
+    var output = '';
+    var api = document.querySelector('input#api-link').value;
+    var name = new Array();
+    document.querySelectorAll('input.api-name').forEach(element => {
+        name.push(element.value);
+    });
+    var varGrab = new Array();
+    document.querySelectorAll('input.variable-grab').forEach(element => {
+        varGrab.push(element.value);
+    });
+    output = 'var ' + name[0] + 'Request = new XMLHttpRequest();' + name[0] + 'Request.open(\'GET\',\'' + api + '\',true);';
+    name.forEach(nameI => {
+        output += 'var ' + nameI + ';'
+    });
+    output += name[0] + 'Request.onload = function () {<br>';
+    varGrab.forEach(grab => {
+        output += '<indent></indent>' + name[varGrab.indexOf(grab)] + ' = JSON.parse(this.response)' + grab + ';';
+    });
+    output += '};' + name[0] + 'Request.send();';
+    output = output.replaceAll(';', ';<br>');
+    document.querySelector('#result').innerHTML = output;
+}
+document.querySelector('button#new').onclick = function () {
+    document.querySelector('div#inputs').innerHTML += '<input class="api-name" placeholder="Name for the variable"><input class="variable-grab" placeholder="Variable extension">';
+};
